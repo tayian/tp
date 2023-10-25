f0f30872b00c3cc4321d96f0b73b5fb7021ceb47
tayian <90464339+tayian@users.noreply.github.com>
2023-10-25 23:24:10 +0800

    Update SampleDataUtil.java

--- src/main/java/connectify/model/util/SampleDataUtil.java
+++ src/main/java/connectify/model/util/SampleDataUtil.java
@@ -6,12 +6,11 @@ import java.util.stream.Collectors;
 
 import connectify.model.AddressBook;
 import connectify.model.ReadOnlyAddressBook;
-import connectify.model.company.Company;
-import connectify.model.person.Address;
-import connectify.model.person.Email;
-import connectify.model.person.Name;
 import connectify.model.person.Person;
-import connectify.model.person.Phone;
+import connectify.model.person.PersonAddress;
+import connectify.model.person.PersonEmail;
+import connectify.model.person.PersonName;
+import connectify.model.person.PersonPhone;
 import connectify.model.tag.Tag;
 
 /**
@@ -20,32 +19,24 @@ import connectify.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
+            new Person(new PersonName("Alex Yeoh"), new PersonPhone("87438807"), new PersonEmail("alexyeoh@example.com"),
+                new PersonAddress("Blk 30 Geylang Street 29, #06-40"),
                 getTagSet("friends")),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+            new Person(new PersonName("Bernice Yu"), new PersonPhone("99272758"), new PersonEmail("berniceyu@example.com"),
+                new PersonAddress("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                 getTagSet("colleagues", "friends")),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+            new Person(new PersonName("Charlotte Oliveiro"), new PersonPhone("93210283"), new PersonEmail("charlotte@example.com"),
+                new PersonAddress("Blk 11 Ang Mo Kio Street 74, #11-04"),
                 getTagSet("neighbours")),
-            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+            new Person(new PersonName("David Li"), new PersonPhone("91031282"), new PersonEmail("lidavid@example.com"),
+                new PersonAddress("Blk 436 Serangoon Gardens Street 26, #16-43"),
                 getTagSet("family")),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"),
+            new Person(new PersonName("Irfan Ibrahim"), new PersonPhone("92492021"), new PersonEmail("irfan@example.com"),
+                new PersonAddress("Blk 47 Tampines Street 20, #17-35"),
                 getTagSet("classmates")),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"),
+            new Person(new PersonName("Roy Balakrishnan"), new PersonPhone("92624417"), new PersonEmail("royb@example.com"),
+                new PersonAddress("Blk 45 Aljunied Street 85, #11-31"),
                 getTagSet("colleagues"))
-        };
-    }
-
-    public static Company[] getSampleCompanies() {
-        return new Company[] {
-            new Company("Unassigned", "Unassigned", "Unassigned", "Unassigned",
-                    "Unassigned", "Unassigned",
-                "Unassigned", "Unassigned"), // To be protected from deletion in future
         };
     }
 
@@ -53,9 +44,6 @@ public class SampleDataUtil {
         AddressBook sampleAb = new AddressBook();
         for (Person samplePerson : getSamplePersons()) {
             sampleAb.addPerson(samplePerson);
-        }
-        for (Company sampleCompany : getSampleCompanies()) {
-            sampleAb.addCompany(sampleCompany);
         }
         return sampleAb;
     }
@@ -68,4 +56,5 @@ public class SampleDataUtil {
                 .map(Tag::new)
                 .collect(Collectors.toSet());
     }
+
 }
