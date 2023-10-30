9e9fa7f89857c7b315e4af57ff0d002148503821
tayian <90464339+tayian@users.noreply.github.com>
2023-10-30 09:37:20 +0800

    Resolve checkstyle issues

--- src/main/java/connectify/logic/commands/ShareCompanyCommand.java
+++ src/main/java/connectify/logic/commands/ShareCompanyCommand.java
@@ -1,15 +1,15 @@
 package connectify.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
 import connectify.commons.core.index.Index;
 import connectify.commons.util.ToStringBuilder;
 import connectify.logic.Messages;
 import connectify.logic.commands.exceptions.CommandException;
 import connectify.model.Model;
 import connectify.model.company.Company;
-
-import java.util.List;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Shares a company to another address book.
@@ -18,7 +18,8 @@ public class ShareCompanyCommand extends Command {
 
     public static final String COMMAND_WORD = "shareCompany";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shares instructions on how to add a Company to another address book.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Shares instructions on how to add a Company to another address book.\n"
             + "Parameters: "
             + "INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
@@ -50,6 +51,12 @@ public class ShareCompanyCommand extends Command {
                 formatInput(Messages.format(companyToShare))));
     }
 
+    /**
+     * Searches for Company information, and formats the output to the user.
+     * 
+     * @param input Input to be formatted.
+     * @return Formatted output.
+     */
     public static String formatInput(String input) {
         String[] parts = input.split("; ");
         StringBuilder result = new StringBuilder("addCompany ");
@@ -60,30 +67,30 @@ public class ShareCompanyCommand extends Command {
             String value = keyValue.length > 1 ? keyValue[1] : "";
 
             switch (key) {
-                case "Industry":
-                    result.append("i/").append(value).append(" ");
-                    break;
-                case "Location":
-                    result.append("l/").append(value).append(" ");
-                    break;
-                case "Description":
-                    result.append("d/").append(value).append(" ");
-                    break;
-                case "Website":
-                    result.append("w/").append(value).append(" ");
-                    break;
-                case "Email":
-                    result.append("e/").append(value).append(" ");
-                    break;
-                case "Phone":
-                    result.append("p/").append(value).append(" ");
-                    break;
-                case "Address":
-                    result.append("a/").append(value).append(" ");
-                    break;
-                default:
-                    result.append("n/").append(key).append(" ");
-                    break;
+            case "Industry":
+                result.append("i/").append(value).append(" ");
+                break;
+            case "Location":
+                result.append("l/").append(value).append(" ");
+                break;
+            case "Description":
+                result.append("d/").append(value).append(" ");
+                break;
+            case "Website":
+                result.append("w/").append(value).append(" ");
+                break;
+            case "Email":
+                result.append("e/").append(value).append(" ");
+                break;
+            case "Phone":
+                result.append("p/").append(value).append(" ");
+                break;
+            case "Address":
+                result.append("a/").append(value).append(" ");
+                break;
+            default:
+                result.append("n/").append(key).append(" ");
+                break;
             }
         }
 
